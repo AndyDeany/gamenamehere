82328ea01ee583df451e5ad9559c40111e3cb9ef
AndyDeany <oneandydean@hotmail.com>
2017-07-16 00:45:08 +0100

    Add methods for when buttons on the main menu are pressed. Slight placement of button changes

--- app/src/main/java/com/example/andrew/gamenamehere/MainActivity.java
+++ app/src/main/java/com/example/andrew/gamenamehere/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.andrew.gamenamehere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the Play button is pressed */
+    public void play(View view) {
+        Intent intent = new Intent(this, MainPlayActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the Options button is pressed */
+    public void options(View view) {
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
     }
 }
