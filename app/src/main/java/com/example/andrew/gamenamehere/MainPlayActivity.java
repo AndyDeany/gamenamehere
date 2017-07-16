cdf3f1d452f682ec41045a999458a4318dcbb768
AndyDeany <oneandydean@hotmail.com>
2017-07-16 14:27:49 +0100

    Add method to MainPlayActivity to hide SystemUI and call it in the onCreate() method

--- app/src/main/java/com/example/andrew/gamenamehere/MainPlayActivity.java
+++ app/src/main/java/com/example/andrew/gamenamehere/MainPlayActivity.java
@@ -2,6 +2,7 @@ package com.example.andrew.gamenamehere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPlayActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainPlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_play);
+
+        hideSystemUI();
+    }
+
+    /** Hide the system UI by using Immersive Fullscreen mode (sticky).
+     *
+     * Sticky means that if the swipes down the UI will appear TEMPORARILY,
+     * as opposed to non-sticky mode, where the UI will appear PERMANENTLY.
+     */
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 }
